f36c0f2d0dd11b70d294f8d17f1d8c199abab07b
Renan Simeone <renansimeone@gmail.com>
2023-07-31 19:21:43 -0300

    feat(logout) :sparkles: Cria o logout(botão sair) e deleta o cookie/token

--- src/app/shared/components/toolbar-nagivation/toolbar-nagivation.component.ts
+++ src/app/shared/components/toolbar-nagivation/toolbar-nagivation.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-toolbar-nagivation',
   templateUrl: './toolbar-nagivation.component.html',
   styleUrls: [],
 })
-export class ToolbarNagivationComponent {}
+export class ToolbarNagivationComponent {
+  constructor(private cookie: CookieService, private router: Router) {}
+  handleLogout(): void {
+    this.cookie.delete('USER_INFO');
+    void this.router.navigate(['/home']);
+  }
+}
