32d3c9ea49ff4faf780eb4b6e82eb89fdc8fb9f3
Renan Simeone <renansimeone@gmail.com>
2023-08-01 20:00:22 -0300

    feat(products-data-transfer) :sparkles: Trabalhando com subjects.

--- src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -1,8 +1,43 @@
-import { Component } from '@angular/core';
+import { ProductsDataTransferService } from './../../../../shared/services/products/products-data-transfer.service';
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/get-all-products-response';
+import { ProductsService } from 'src/app/services/products/products.service';
 
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
   styleUrls: [],
 })
-export class DashboardHomeComponent {}
+export class DashboardHomeComponent implements OnInit {
+  public productsList: Array<GetAllProductsResponse> = [];
+  constructor(
+    private productsService: ProductsService,
+    private messageService: MessageService,
+    private productsDtService: ProductsDataTransferService
+  ) {}
+
+  ngOnInit(): void {
+    this.getProductsDatas();
+  }
+
+  getProductsDatas(): void {
+    this.productsService.getAllProducts().subscribe({
+      next: (response) => {
+        if (response.length > 0) {
+          this.productsList = response;
+          this.productsDtService.setProductsDatas(this.productsList);
+        }
+      },
+      error: (err) => {
+        console.log(err);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Erro',
+          detail: 'Erro ao buscar produtos!',
+          life: 2500,
+        });
+      },
+    });
+  }
+}
--- src/app/services/products/products.service.ts
+++ src/app/services/products/products.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { environment } from 'src/app/environments/environment';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/get-all-products-response';
 
@@ -20,9 +20,11 @@ export class ProductsService {
   constructor(private http: HttpClient, private cookie: CookieService) {}
 
   getAllProducts(): Observable<Array<GetAllProductsResponse>> {
-    return this.http.get<Array<GetAllProductsResponse>>(
-      `${this.API_URL}/products`,
-      this.httpOptions
-    );
+    return this.http
+      .get<Array<GetAllProductsResponse>>(
+        `${this.API_URL}/products`,
+        this.httpOptions
+      )
+      .pipe(map((porduct) => porduct.filter((data) => data.amount > 0)));
   }
 }
