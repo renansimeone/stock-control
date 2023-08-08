3f85e993c1952364dca1df0f8e5b43450a3c8de4
Renan Simeone <renansimeone@gmail.com>
2023-08-08 20:08:03 -0300

    feat(dashboard-home/home) :sparkles: Implementando estratégia para evitar Memory Leak

--- src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -1,6 +1,7 @@
 import { ProductsDataTransferService } from './../../../../shared/services/products/products-data-transfer.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/get-all-products-response';
 import { ProductsService } from 'src/app/services/products/products.service';
 
@@ -9,7 +10,8 @@ import { ProductsService } from 'src/app/services/products/products.service';
   templateUrl: './dashboard-home.component.html',
   styleUrls: [],
 })
-export class DashboardHomeComponent implements OnInit {
+export class DashboardHomeComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
   public productsList: Array<GetAllProductsResponse> = [];
   constructor(
     private productsService: ProductsService,
@@ -17,27 +19,35 @@ export class DashboardHomeComponent implements OnInit {
     private productsDtService: ProductsDataTransferService
   ) {}
 
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
   ngOnInit(): void {
     this.getProductsDatas();
   }
 
   getProductsDatas(): void {
-    this.productsService.getAllProducts().subscribe({
-      next: (response) => {
-        if (response.length > 0) {
-          this.productsList = response;
-          this.productsDtService.setProductsDatas(this.productsList);
-        }
-      },
-      error: (err) => {
-        console.log(err);
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Erro',
-          detail: 'Erro ao buscar produtos!',
-          life: 2500,
-        });
-      },
-    });
+    this.productsService
+      .getAllProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.productsList = response;
+            this.productsDtService.setProductsDatas(this.productsList);
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao buscar produtos!',
+            life: 2500,
+          });
+        },
+      });
   }
 }
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,9 +1,10 @@
 import { UserService } from './../../services/user/user.service';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { MessageService } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
 import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
 import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
 
@@ -12,7 +13,8 @@ import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy {
+  private destroy$ = new Subject<void>();
   loginCard = true;
   loginForm = this.formBuilder.group({
     email: ['', Validators.required],
@@ -32,23 +34,31 @@ export class HomeComponent {
     private router: Router
   ) {}
 
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
   onSubmitLoginForm(): void {
     if (this.loginForm.value && this.loginForm.valid) {
-      this.userService.authUser(this.loginForm.value as AuthRequest).subscribe({
-        next: (response) => {
-          if (response) {
-            this.cookieService.set('USER_INFO', response?.token);
-            this.loginForm.reset();
-            this.router.navigate(['/dashboard']);
-            this.messageService.add({
-              severity: 'success',
-              summary: 'Sucesso',
-              detail: `Bem vindo de volta ${response?.name}`,
-            });
-          }
-        },
-        error: (err) => console.log(err),
-      });
+      this.userService
+        .authUser(this.loginForm.value as AuthRequest)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              this.cookieService.set('USER_INFO', response?.token);
+              this.loginForm.reset();
+              this.router.navigate(['/dashboard']);
+              this.messageService.add({
+                severity: 'success',
+                summary: 'Sucesso',
+                detail: `Bem vindo de volta ${response?.name}`,
+              });
+            }
+          },
+          error: (err) => console.log(err),
+        });
     }
     console.log('Dados do formulário de login:', this.loginForm.value);
   }
@@ -57,6 +67,7 @@ export class HomeComponent {
     if (this.signupForm.value && this.signupForm.valid) {
       this.userService
         .signupUser(this.signupForm.value as SignupUserRequest)
+        .pipe(takeUntil(this.destroy$))
         .subscribe({
           next: (response) => {
             if (response) {
